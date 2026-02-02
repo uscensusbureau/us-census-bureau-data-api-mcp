bd1fcf7643b1d175d2e5800e9ac69cb21baceaa0
Luke Keller <luke.keller@census.gov>
2026-02-02 16:08:00 -0500

    Add Import Geographies Flag to Seeded Years
    Adds import_geographies (true) value to imported years (2020, 2023) to ensure those vintages are used to import geographic data.
    
    * Add import_geographies (true) value to years.json data
    * Update YearSchema to include import_geographies

--- mcp-db/src/schema/year.schema.ts
+++ mcp-db/src/schema/year.schema.ts
@@ -2,6 +2,7 @@ import { z } from 'zod'
 
 export const YearSchema = z.object({
   year: z.int().min(1776),
+  import_geographies: z.boolean().default(false),
 })
 
 export const YearsArraySchema = z.array(YearSchema)
--- mcp-db/tests/schema/year.schema.test.ts
+++ mcp-db/tests/schema/year.schema.test.ts
@@ -1,26 +1,30 @@
 import { describe, expect, it } from 'vitest'
 import { YearsArraySchema, YearSchema } from '../../src/schema/year.schema'
+import { z } from 'zod'
 
 describe('SummaryLevelSchema', () => {
   it('should validate a Summary Level object', () => {
     const year = {
-      year: 2020,
+      year: 2020
     }
 
     const result = YearSchema.safeParse(year)
 
     expect(result.success).toBe(true)
+    expect(result.data.import_geographies).toBe(false)
   })
 })
 
 describe('YearsArraySchema', () => {
   it('should validate a Summary Level object', () => {
-    const years: YearSchema[] = [
+    const years: z.infer<typeof YearSchema>[] = [
       {
         year: 2020,
+        import_geographies: true,
       },
       {
         year: 2023,
+        import_geographies: true,
       },
     ]
 
